0a1913ae6865c0f5c103a7f2144466d8b896be09
dreamer <christopherlu.pdx@gmail.com>
2019-08-12 22:12:27 -0700

    fixed ajax

--- stuff.js
+++ stuff.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    $('form').submit(function(forminfo){
-        console.log('here')
-        forminfo.preventDefault();
+    $('form').submit(function(event){
+        event.preventDefault();
         var data = $('#loginform').serialize();
-        console.log(data)
-    })
-    $.ajax({
-        type: 'POST',
-            url: 'something.php',
-            data: data,
-            dataType: 'json',
-            success: function(d) {
+        console.log(data);
+        $.ajax({
+            type: 'POST'
+            , url: './something.php'
+            , data: data
+            , dataType: 'json'
+            , success: function(d) {
                 console.log(d);
             }
+        })
     })
+
 })
